ea41c2a139989b5e69ed174028c3edd846bc749e
LenaZaitseva <elena.ivanova013@gmail.com>
2020-04-10 17:32:23 +0300

    Some changes

--- App.js
+++ App.js
@@ -1,15 +1,12 @@
 import React, { useState } from 'react'
-import {Alert, StyleSheet, View} from 'react-native'
+import { StyleSheet, View, Alert } from 'react-native'
 import { Navbar } from './src/components/Navbar'
 import { MainScreen } from './src/screens/MainScreen'
 import { TodoScreen } from './src/screens/TodoScreen'
 
 export default function App() {
-  const [todoId, setTodoId] = useState('2');
-  const [todos, setTodos] = useState([
-    { id: '1', title: 'Выучить React Native' },
-    { id: '2', title: 'Написать приложение' }
-  ]);
+  const [todoId, setTodoId] = useState(null);
+  const [todos, setTodos] = useState([]);
 
   const addTodo = title => {
     setTodos(prev => [
@@ -23,7 +20,6 @@ export default function App() {
 
   const removeTodo = id => {
     const todo = todos.find(t => t.id === id);
-
     Alert.alert(
         'Удаление элемента',
         `Вы уверены, что хотите удалить "${todo.title}"?`,
@@ -37,7 +33,7 @@ export default function App() {
             style: 'destructive',
             onPress: () => {
               setTodoId(null);
-              setTodos(prev => prev.filter(todo => todo.id !== id));
+              setTodos(prev => prev.filter(todo => todo.id !== id))
             }
           }
         ],
@@ -45,29 +41,44 @@ export default function App() {
     )
   };
 
+  const updateTodo = (id, title) => {
+    setTodos(old =>
+        old.map(todo => {
+          if (todo.id === id) {
+            todo.title = title
+          }
+          return todo
+        })
+    )
+  };
+
   let content = (
-    <MainScreen
-      todos={todos}
-      addTodo={addTodo}
-      removeTodo={removeTodo}
-      openTodo={setTodoId}
-    />
+      <MainScreen
+          todos={todos}
+          addTodo={addTodo}
+          removeTodo={removeTodo}
+          openTodo={setTodoId}
+      />
   );
 
   if (todoId) {
     const selectedTodo = todos.find(todo => todo.id === todoId);
-    content = <TodoScreen
-        onRemove={removeTodo}
-        goBack={() => setTodoId(null)}
-        todo={selectedTodo}
-    />
+
+    content = (
+        <TodoScreen
+            onRemove={removeTodo}
+            goBack={() => setTodoId(null)}
+            todo={selectedTodo}
+            onSave={updateTodo}
+        />
+    )
   }
 
   return (
-    <View>
-      <Navbar title='Todo App!' />
-      <View style={styles.container}>{content}</View>
-    </View>
+      <View>
+        <Navbar title='Todo App!' />
+        <View style={styles.container}>{content}</View>
+      </View>
   )
 }
 
